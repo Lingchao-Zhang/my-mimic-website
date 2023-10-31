56acbc75decb9ede6f1487a94866dafc56fd0809
Lingchao-Zhang <108935399+Lingchao-Zhang@users.noreply.github.com>
2023-10-31 15:37:34 +1100

    Car catalogue (#2)
    * SearchBar and SearchManufacturer add:
    1. add SearchManufacturer that can find all manufacturers matching the query.
    2. add a new type searchManufacturerType.
    3. install headlessui.
    4. update index.ts in components folder.
    
    * style add: the last item selected will be bold

---
+++ my-mimic-app/components/CustomFilter.tsx
@@ -0,0 +1,8 @@
+"use client"
+const CustomFilter = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default CustomFilter
--- my-mimic-app/components/index.ts
+++ my-mimic-app/components/index.ts
@@ -1,8 +1,12 @@
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import CustomFilter from "./CustomFilter"
+import SearchBar from "./SearchBar";
 export {
     Hero,
     Navbar,
-    Footer
+    Footer,
+    CustomFilter,
+    SearchBar
 }
